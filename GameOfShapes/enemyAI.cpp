e5b2b53f0d14a8db4544733df970d09a0e407b76
maianhpham123 <23021469@vnu.edu.vn>
2024-04-23 23:08:44 +0700

    added to repo

---
+++ GameOfShapes/enemyAI.cpp
@@ -0,0 +1,8 @@
+//
+//  enemyAI.cpp
+//  GameOfShapes
+//
+//  Created by Mai Anh Pham on 4/23/24.
+//
+
+#include "enemyAI.hpp"
---
+++ GameOfShapes/enemyAI.hpp
@@ -0,0 +1,13 @@
+//
+//  enemyAI.hpp
+//  GameOfShapes
+//
+//  Created by Mai Anh Pham on 4/23/24.
+//
+
+#ifndef enemyAI_hpp
+#define enemyAI_hpp
+
+//TODO: create AI 
+
+#endif /* enemyAI_hpp */
